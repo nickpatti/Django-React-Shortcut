f42d92f86833be29eabdb9441c7d68faf3d695ed
nickpatti <59921028+nickpatti@users.noreply.github.com>
2020-08-31 14:17:42 +0100

    Update tests.py

--- users/tests.py
+++ users/tests.py
@@ -1,5 +1,5 @@
-from rest_framework.authtoken.models import Token
-from rest_framework.test import APIRequestFactory
+from knox.models import AuthToken
+from rest_framework.test import APIRequestFactory, APIClient
 from rest_framework.test import APITestCase
 from rest_framework.test import force_authenticate
 from django.contrib.auth.models import User
@@ -9,15 +9,18 @@ from .api import LoginAPI, RegisterAPI, UserAPI
 class LoginAPITest(APITestCase):
     def setUp(self):
         self.factory = APIRequestFactory()
+        self.client = APIClient()
         self.user = User.objects.create_user(
             username='user@foo.com', email='user@foo.com', password='top_secret')
-        self.token = Token.objects.create(user=self.user)
-        self.token.save()
+        self.token = AuthToken.objects.create(user=self.user)
 
     def test_token_auth(self):
-        request = self.factory.post('/api/auth/login', {'username': 'user@foo.com', 'email': 'user@foo.com', 'password': 'top_secret'})
-        # force_authenticate(request, token=self.token.key)
+        request = self.factory.post('/api/auth/login', {'username': 'user@foo.com', 'email': 'user@foo.com', 'password': 'top_secret', 'token': self.token})
         view = LoginAPI.as_view()
         response = view(request)
         self.assertEqual(response.status_code, 200)
-        # json_response = json.loads(response.render().content)['results']
+
+    def test_get_user(self):
+        self.client.force_authenticate(self.user)
+        request = self.client.get('/api/auth/user')
+        self.assertEqual(request.status_code, 200)
